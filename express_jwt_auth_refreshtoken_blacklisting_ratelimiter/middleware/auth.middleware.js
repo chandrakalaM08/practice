890be5a0076d8c9907e02f5137225bed043a9664
chandrakalaM08 <chandrakala.2508@gmail.com>
2023-07-10 02:59:29 +0530

    blacklisting

--- express_jwt_auth_refreshtoken_blacklisting_ratelimiter/middleware/auth.middleware.js
+++ express_jwt_auth_refreshtoken_blacklisting_ratelimiter/middleware/auth.middleware.js
@@ -1,12 +1,23 @@
 const secretKey = process.env.secretKey;
 
 var jwt = require("jsonwebtoken");
+const BlacklistedTokenModel = require("../models/blacklistedModel");
+
 // Middleware to verify JWT token
-const verifyToken = (req, res, next) => {
-  // When your token is stored in cookies
-  const { token } = req.cookies;
-  console.log("token inside middleware", token);
+const verifyToken = async (req, res, next) => {
   try {
+    // When your token is stored in cookies
+    const { token } = req.cookies;
+    console.log("token inside middleware", token);
+    // Check if the token is blacklisted
+    const blacklistedToken = await BlacklistedTokenModel.find({});
+
+    if (blacklistedToken.includes(token)) {
+      return res.status(401).json({
+        message: "Invalid token , already exists in blacklist.",
+        error: error.message,
+      });
+    }
     const decoded = jwt.verify(token, secretKey);
     req.user = decoded;
     next();
